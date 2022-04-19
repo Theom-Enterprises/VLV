5eb145f65748301dd6314d2d2c20e1bf14aa4afa
Daniel Lozada <daniellozada1702@gmail.com>
2022-04-19 08:52:43 +0200

    Bootstrap Card Styling

--- index.php
+++ index.php
@@ -2,15 +2,13 @@
 
 require_once('vendor/autoload.php');
 
-use TYPO3Fluid\Fluid\View\TemplateView;
-
 use Theom\Vlv\Hotel;
 
 $hotels[] = new Hotel('Joker', 5, 'Die größte Rutsche der Welt', '15.5 - 10.9 2-24 Uhr');
 $hotels[] = new Hotel('Ass', 3, 'Ein schwebender Swimmingpool', '11.6 - 10.11 2-24 Uhr');
 $hotels[] = new Hotel('King', 4, 'Pool-Tisch im Pool #Poolception', 'Rund um die Uhr 365 Tage im Jahr');
 
-$view = new TemplateView();
+$view = new \TYPO3Fluid\Fluid\View\TemplateView();
 $paths = $view->getTemplatePaths();
 $paths->setTemplatePathAndFilename('templates/main.html');
 $view->assignMultiple(
